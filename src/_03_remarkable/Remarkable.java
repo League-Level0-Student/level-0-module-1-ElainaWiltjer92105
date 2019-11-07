ca4eb47728e22b09424f7056b885b56c369edc77
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-07 13:28:12 -0800

    Mustaches

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -9,10 +9,35 @@ public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
 
+	String aboutJoseline= " There nice";
+	String aboutKarla= " There funny";
+	String aboutChris= "There tall";
+	
+	
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
+	String name= JOptionPane.showInputDialog("What is your name? ");
+	
+	
+	
+	
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
+	 if ( name.equals("Karla")) { 
+		JOptionPane.showMessageDialog(null, aboutKarla); 
+		 
+		
+	 }
+	if ( name.equals("Chris")) { 
+		JOptionPane.showMessageDialog(null, aboutChris);
+		
+		
+	}
+	if ( name.equals("Joseline")) {
+		JOptionPane.showMessageDialog(null, aboutJoseline);
+		
+	}
+	 
 	}
 }
 
